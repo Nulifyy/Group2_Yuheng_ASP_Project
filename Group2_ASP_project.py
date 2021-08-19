3fa607de208970ee4a63971caa622d2c6962435c
Lionel71 <lionelloh126@gmail.com>
2021-08-19 15:55:44 +0800

    1

--- Group2_ASP_project.py
+++ Group2_ASP_project.py
@@ -1,1 +1,18 @@
-print("HAO TIAN MIGHT BE GAY")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_excel('IMVA.xls',sheet_name="IMVA")
+ds = data['Periods'].str.split(' ',n=1,expand = True)
+
+data = data.assign(Year=ds[0])
+data.index = data['Year']
+print(data)
+
+Asia2 = data[(data['Year'] >= str(1988)) & (data['Year'] <= str(1997))];
+print(Asia2.columns)
+
+dict = {"country":['Brunei Darussalam', 'Indonesia', 'Malaysia', 'Myanmar', 'Philippines', 'Thailand', 'Vietnam', 'China', 'Hong Kong SAR', 'Taiwan', 'Japan', 'South Korea', 'Bangladesh', 'India', 'Pakistan', 'Sri Lanka', 'Iran', 'Kuwait', 'Israel', 'Saudi Arabia', 'United Arab Emirates']}
+
+df = pd.DataFrame(dict)
+print(df)
